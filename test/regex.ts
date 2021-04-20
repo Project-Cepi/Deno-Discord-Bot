ed803df0c8a8e41c66a005a0f0bc233f02feb57a
LeoDog896 <LeoDog896@hotmail.com>
2021-04-20 09:14:06 -0400

    Properly verify regex

--- test/regex.ts
+++ test/regex.ts
@@ -1,10 +1,22 @@
 import { regex } from 'src/chatbot/regexVerifier.ts'
 import { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
 
+const userInput = "how are you?"
+
 Deno.test({
 	name: "check if regex verifies matches correctly",
 	fn: () => {
-		const x = 1 + 2;
-		assertEquals(x, 3);
+
+		const assertRegexMatches = (input: string, expected: string = userInput) => assertEquals(input.replace(regex, ""), userInput)
+
+		assertRegexMatches(`ok cepi ${userInput}`, userInput)
+		assertRegexMatches(`ok cepi, ${userInput}`, userInput)
+
+		assertRegexMatches(`hey cepi ${userInput}`, userInput)
+		assertRegexMatches(`hey cepi, ${userInput}`, userInput)
+
+		assertRegexMatches(`cepi ${userInput}`, userInput)
+		assertRegexMatches(`cepi, ${userInput}`, userInput)
+
 	},
 });
