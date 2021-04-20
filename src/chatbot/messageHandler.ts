74bb31567c5d7eb4a5f2019789b8a84fc11c2678
LeoDog896 <LeoDog896@hotmail.com>
2021-04-20 11:35:16 -0400

    woo closest match

--- src/chatbot/messageHandler.ts
+++ src/chatbot/messageHandler.ts
@@ -1,12 +1,27 @@
 import type { Message } from "discordeno";
 import { data } from './dataProcessor.ts';
+import { matchPercentage } from './utils/closestMatch.ts'
 
 export async function handleMessage(processedText: string, originalMessage: Message) {
 
 	await originalMessage.addReaction("📝")
 
-	await originalMessage.reply(processedText);
+	const matchedKey = [...data.keys()].sort((a, b) => matchPercentage(a, processedText) - matchPercentage(b, processedText))[0]
 
-	data
+	if (!matchedKey) {
+		await originalMessage.reply("For some reason, no keys were found?")
+		return;
+	}
+
+	const match = data.get(matchedKey)
+
+	if (!match) {
+		await originalMessage.reply("This shouldn't happen.")
+		return;
+	}
+
+	const randomMatch = match[Math.floor(Math.random() * match.length)]; // "random match"
+
+	await originalMessage.reply(randomMatch);
 	
 }
---
+++ src/chatbot/utils/closestMatch.ts
@@ -0,0 +1,31 @@
+export function matchPercentage(first: string, second: string): number {
+	if (first.length == 0) return first.length
+	if (second.length == 0) return second.length 
+
+	const matrix = [];
+
+	// increment along the first column of each row
+	for (let i = 0; i <= second.length; i++){
+		matrix[i] = [i];
+	}
+
+	// increment each column in the first row
+	for (let j = 0; j <= first.length; j++){
+		matrix[0][j] = j;
+	}
+
+	// Fill in the rest of the matrix
+	for (let i = 1; i <= second.length; i++){
+		for (let j = 1; j <= first.length; j++){
+			if (second.charAt(i - 1) == first.charAt(j - 1)){
+				matrix[i][j] = matrix[i-1][j-1];
+			} else {
+				matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+					Math.min(matrix[i][j-1] + 1, // insertion
+					matrix[i-1][j] + 1)); // deletion
+			}
+		}
+	}
+
+	return matrix[second.length][first.length];
+}
