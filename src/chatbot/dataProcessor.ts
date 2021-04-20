8fad14080ddccb027bb5d3373122eeacbc1d6182
LeoDog896 <LeoDog896@hotmail.com>
2021-04-20 11:42:07 -0400

    Allow scanning of data dir for data

--- src/chatbot/dataProcessor.ts
+++ src/chatbot/dataProcessor.ts
@@ -2,10 +2,15 @@ import { parse } from "toml";
 
 export const data: Map<string, string[]> = new Map();
 
-const greetings = parse(await Deno.readTextFile("data/greetings.toml"))
+for await (const fileEntry of Deno.readDir("data")) {
 
-Object.keys(greetings).forEach(key => {
-	if (!greetings[key]) return;
+	if (fileEntry.isDirectory) continue;
 
-	data.set(key, greetings[key] as string[])
-})
+	const file = parse(await Deno.readTextFile("data/" + fileEntry.name))
+
+	Object.keys(file).forEach(key => {
+		if (!file[key]) return;
+
+		data.set(key, file[key] as string[])
+	})
+}
