3d7d29894f9aefc4535393ad489db7a3479c4443
LeoDog896 <LeoDog896@hotmail.com>
2021-04-20 07:13:42 -0400

    React with a little write emoji!

--- src/chatbot/messageHandler.ts
+++ src/chatbot/messageHandler.ts
@@ -1,5 +1,8 @@
 import type { Message } from "discordeno";
 
 export async function handleMessage(processedText: string, originalMessage: Message) {
+
+	await originalMessage.addReaction("📝")
+
 	await originalMessage.reply(processedText);
 }
