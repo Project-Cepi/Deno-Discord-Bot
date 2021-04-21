a15c172b2718baaf023e2f46e63bfced669ba077
LeoDog896 <LeoDog896@hotmail.com>
2021-04-21 19:45:19 -0400

    money data

--- src/chatbot/dataProcessor.ts
+++ src/chatbot/dataProcessor.ts
@@ -1,4 +1,5 @@
 import { parse } from "toml";
+import * as log from "logger";
 
 export const data: Map<string, string[]> = new Map();
 
@@ -7,6 +8,8 @@ for await (const fileEntry of Deno.readDir("data")) {
 	if (fileEntry.isDirectory) continue;
 
 	const file = parse(await Deno.readTextFile("data/" + fileEntry.name))
+
+	log.info("loading " + fileEntry.name)
 
 	Object.keys(file).forEach(key => {
 		if (!file[key]) return;
