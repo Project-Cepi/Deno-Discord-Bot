f058645df66b0aa075967c024281b18764c8ca94
LeoDog896 <LeoDog896@hotmail.com>
2021-04-22 19:30:11 -0400

    Remove debug

--- src/chatbot/utils/closestMatch.ts
+++ src/chatbot/utils/closestMatch.ts
@@ -10,7 +10,5 @@ export function closestMatch(input: string, possibleStrings: string[]): string {
 
 	const fuse = new Fuse(possibleStrings, options, null)
 
-	console.log(fuse.search(input))
-
 	return fuse.search(input)[0].item
 }
